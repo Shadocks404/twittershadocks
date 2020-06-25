1775e54a2d3cd7687371badaf1fdb282358bcae0
Pierre-Louis Deshayes <pierre-louis@macbook-pro.home>
2020-06-25 19:18:12 +0200

    initial commit

---
+++ precache-manifest.588a616c6ed435e90d79dd3cd2d9c8c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bbdbd269190bfb4f001e8000c90cd9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4de45f3ccc6ec1419794",
+    "url": "./static/css/2.e9852964.chunk.css"
+  },
+  {
+    "revision": "17e7318141b9ccf598c7",
+    "url": "./static/css/main.52617fb8.chunk.css"
+  },
+  {
+    "revision": "4de45f3ccc6ec1419794",
+    "url": "./static/js/2.4e24c14a.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "./static/js/2.4e24c14a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e7318141b9ccf598c7",
+    "url": "./static/js/main.ffdfe218.chunk.js"
+  },
+  {
+    "revision": "d02b4d295322dfd980bf",
+    "url": "./static/js/runtime-main.c8db80e6.js"
+  }
+]);
